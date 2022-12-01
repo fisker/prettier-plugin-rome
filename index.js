38a2ec1054049b4935ece969a15f7f915f9903b6
fisker Cheung <lionkay@gmail.com>
2022-12-01 10:01:23 +0800

    Add willPrintOwnComments

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import format from './rome-format.js'
-import languages from './languages.js'
-import getRomeConfiguration from './get-rome-configuration.js'
+import format from './rome-format.js';
+import languages from './languages.js';
+import getRomeConfiguration from './get-rome-configuration.js';
 
-const parserName = 'rome'
-const astFormat = parserName
+const parserName = 'rome';
+const astFormat = parserName;
 
 const parse = async (text, options) => ({
   text: await format(text, getRomeConfiguration(options), options.filepath),
   range: [0, text.length],
-})
+});
 
-const print = (path) => path.node.text
+const print = (path) => path.node.text;
 
 export default {
   languages: languages.map((language) => ({
@@ -26,6 +26,9 @@ export default {
     },
   },
   printers: {
-    [astFormat]: {print},
+    [astFormat]: {
+      print,
+      willPrintOwnComments: () => true,
+    },
   },
-}
+};
